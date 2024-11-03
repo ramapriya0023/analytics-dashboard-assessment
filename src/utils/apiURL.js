dc35d1a6a2f376d3473bb877de13af4d94e26ce3
ramapriya0023 <priya.rama0023@gmail.com>
2024-11-04 00:28:21 +0530

    Updated api url

--- src/utils/apiURL.js
+++ src/utils/apiURL.js
@@ -1,2 +1,4 @@
 export const apiUrl =
-  process.env.NODE_ENV === "production" ? "/api" : "http://localhost:8000/api";
+  process.env.NODE_ENV === "production"
+    ? "https://analytics-dashboard-assessment-umber.vercel.app/api"
+    : "http://localhost:8000/api";
